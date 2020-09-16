7c572d4d12265f50a33a2c9c0c3880f89a207a38
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-09-16 15:28:52 +0300

    add custom user and password validation, realizate method edid and delete

---
+++ Users/Infrastructure/CustomUserValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Users.Models;
+
+namespace Users.Infrastructure
+{
+    public class CustomUserValidator: IUserValidator<AppUser>
+    {
+        public Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user)
+        {
+            if (user.Email.ToLower().EndsWith("@gmail.com"))
+            {
+                return Task.FromResult(IdentityResult.Success);
+            }
+            else
+            {
+                return Task.FromResult(IdentityResult.Failed(new IdentityError
+                {
+                    Code = "EmailDomenError",
+                    Description = "Only @gmail.com email addresses are allowed"
+                }));
+            }
+        }
+    }
+}
--- Users/Startup.cs
+++ Users/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Users.Models;
+using Users.Infrastructure;
 
 namespace Users
 {
@@ -21,8 +22,23 @@ namespace Users
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //добавляем специальные проверки юзер и паролей созданые нами
+            services.AddTransient<IPasswordValidator<AppUser>, CustomPasswordValidator>();
+            services.AddTransient<IUserValidator<AppUser>, CustomUserValidator>();
+
             services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(Configuration["Data:SportStoreIdentity:ConnectionString"]));
-            services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
+            //Конфигурация правил проверки паролей  информацыи пользователей
+            services.AddIdentity<AppUser, IdentityRole>(opts =>
+            {
+                opts.User.AllowedUserNameCharacters = "qwertyuiopasdfghjklzxcvbnm";
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppIdentityDbContext>().AddDefaultTokenProviders();
+
             services.AddMvc();
         }
 
