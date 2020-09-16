50ff35822a20519a8f1c5c540b7e612836d247f7
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-09-16 22:22:41 +0300

    add user auntefication

---
+++ Users/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Users.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Users.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private UserManager<AppUser> userManager;
+        private SignInManager<AppUser> signInManager;
+        public AccountController(UserManager<AppUser> userMrg, SignInManager<AppUser> signInMrg)
+        {
+            userManager = userMrg;
+            signInManager = signInMrg;
+        }
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel details, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await userManager.FindByEmailAsync(details.Email);
+                if(user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result =
+                        await signInManager.PasswordSignInAsync(user, details.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError(nameof(LoginModel.Email), "Invalid email or password");
+            }
+            return View(details);
+        }
+    }
+}
--- Users/Controllers/HomeController.cs
+++ Users/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Users.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ViewResult Index() => View(new Dictionary<string, object> { ["Placeholder"] = "Placeholder" });
     }
 }
